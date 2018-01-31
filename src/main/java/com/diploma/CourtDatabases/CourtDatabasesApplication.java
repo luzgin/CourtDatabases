6e63ae0d7ac6cc1f945e7dc398084f76e89e5abd
egor.luzgin <lew.vt1@gmail.com>
2018-01-31 10:58:03 +0300

    Create project

---
+++ src/main/java/com/diploma/CourtDatabases/CourtDatabasesApplication.java
@@ -0,0 +1,12 @@
+package com.diploma.CourtDatabases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtDatabasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourtDatabasesApplication.class, args);
+	}
+}
