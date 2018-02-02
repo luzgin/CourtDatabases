97858af99374a45f629c2d27b30d9de642511b3b
egor.luzgin <lew.vt1@gmail.com>
2018-02-02 14:53:00 +0300

    create html

---
+++ src/main/java/com/diploma/CourtDatabases/configuration/JpaConfiguration.java
@@ -0,0 +1,96 @@
+package com.diploma.CourtDatabases.configuration;
+
+import com.zaxxer.hikari.HikariDataSource;
+import java.util.Properties;
+
+import javax.naming.NamingException;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.diploma.CourtDatabases.repository")
+@EnableTransactionManagement
+public class JpaConfiguration {
+
+
+    @Value("${spring.datasource.hikari.maximum-pool-size:10}")
+    private int maxPoolSize;
+
+    /*
+     * Populate SpringBoot DataSourceProperties object directly from application.yml
+     * based on prefix.Thanks to .yml, Hierachical data is mapped out of the box with matching-name
+     * properties of DataSourceProperties object].
+     */
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSourceProperties dataSourceProperties(){
+        return new DataSourceProperties();
+    }
+
+    /*
+     * Configure HikariCP pooled DataSource.
+     */
+    @Bean
+    public DataSource dataSource() {
+        DataSourceProperties dataSourceProperties = dataSourceProperties();
+        HikariDataSource dataSource = (HikariDataSource) DataSourceBuilder
+                .create(dataSourceProperties.getClassLoader())
+                .driverClassName(dataSourceProperties.getDriverClassName())
+                .url(dataSourceProperties.getUrl())
+                .username(dataSourceProperties.getUsername())
+                .password(dataSourceProperties.getPassword())
+                .type(HikariDataSource.class)
+                .build();
+        dataSource.setMaximumPoolSize(maxPoolSize);
+        return dataSource;
+    }
+
+    /*
+     * Entity Manager Factory setup.
+     */
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataSource());
+       // factoryBean.setPackagesToScan(new String[] { "com.diploma.CourtDatabases.entity" });
+
+        return factoryBean;
+    }
+
+    /*
+     * Here you can specify any provider specific properties.
+     */
+    private Properties jpaProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.format_sql", "true");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+
+        return properties;
+    }
+
+    @Bean
+    @Autowired
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(emf);
+        return txManager;
+    }
+
+}
