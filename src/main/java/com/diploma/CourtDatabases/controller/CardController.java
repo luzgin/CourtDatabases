3d0bbef55cbec3257542bce042f3ed5aae9fb8c7
egor.luzgin <lew.vt1@gmail.com>
2018-03-30 18:01:58 +0300

    create part report

--- src/main/java/com/diploma/CourtDatabases/controller/CardController.java
+++ src/main/java/com/diploma/CourtDatabases/controller/CardController.java
@@ -1,17 +1,22 @@
 package com.diploma.CourtDatabases.controller;
 
 import com.diploma.CourtDatabases.entity.CardAdm;
+import com.diploma.CourtDatabases.entity.ComplaintsAdm;
+import com.diploma.CourtDatabases.entity.DecreeAdm;
 import com.diploma.CourtDatabases.service.CardAdmService;
+import com.diploma.CourtDatabases.service.ComplaintsAdmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api")
 public class CardController {
     @Autowired
     private CardAdmService cardAdmService;
+    @Autowired
+    private ComplaintsAdmService complaintsAdmService;
 
     @GetMapping(value = "/cardAdm/")
     public List<CardAdm> getAllCards() {
@@ -25,6 +30,7 @@ public class CardController {
 
     @PostMapping("/cardAdm/")
     public CardAdm save(@RequestBody CardAdm cardAdm) {
+        System.out.println(cardAdm);
         return cardAdmService.save(cardAdm);
     }
 
@@ -49,5 +55,33 @@ public class CardController {
     @DeleteMapping("/cardAdm/{id}")
     public void deleteEntity(@PathVariable("id") long id) {
         cardAdmService.delete(id);
+    }
+
+    public void qwe(@PathVariable("dateFrom")Date dateFrom, @PathVariable("dateTo")Date dateTo){
+        Integer numberOfCases = 0;
+        List<CardAdm> cardsList = cardAdmService.findByCreateDateBetween(dateFrom,dateTo); //список всех карточек за период
+        HashSet<DecreeAdm> decreeSet = new HashSet<>();
+        for (int i = 0; i < cardsList.size(); i++) {
+            decreeSet.add(cardsList.get(i).getDecreeAdm());   //уникальные постановления из текущих карточек
+        }
+        Iterator <DecreeAdm> iterator = decreeSet.iterator();
+        while (iterator.hasNext()){
+            DecreeAdm currentDecree = iterator.next();
+            List<ComplaintsAdm> complaintsForCurrentDecree = complaintsAdmService.findByDecreeAdm(currentDecree); //постановления для выбранной жалобы
+            if (complaintsForCurrentDecree.size() > 0){
+                if(complaintsForCurrentDecree.size() == 1){
+                    numberOfCases++;   //TODO может понадобится проверка на дату жалобы по постановлению
+                }else {
+                    int nimberOfCasesForCurrentComplaint = 0;
+                    for (int i = 0; i < complaintsForCurrentDecree.size(); i++) {
+                        ComplaintsAdm com = complaintsForCurrentDecree.get(i);
+                        if (com.isReinstatementOfTerm() == false){ //если не восстановление срока
+                                                       
+                        }
+                    }
+                }
+
+            }
+        }
     }
 }
